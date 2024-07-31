1eb2d684b84c6b3d4b249dbd0f1916b8f7f69c57
Louran Costa <louran.costa@yahoo.com.br>
2024-07-31 06:21:27 -0300

    feat: add MongoClient with mongodb connection

--- src/index.ts
+++ src/index.ts
@@ -2,19 +2,27 @@ import express from "express";
 import { config } from "dotenv";
 import { GetPartnersControler } from "./controllers/get-partners/get-partners";
 import { MongoGetPartnersRepository } from "./repositories/get-partners/mongo-get-partners";
+import { MongoClient } from "./database/mongo";
 
-config();
+const main = async () => {
+  config();
 
-const app = express();
-const port = process.env.PORT || 8000;
+  const app = express();
 
-app.get("/partners", async (req, res) => {
-  const mngoGetPartnersRepository = new MongoGetPartnersRepository();
-  const getPartnersController = new GetPartnersControler(
-    mngoGetPartnersRepository
-  );
-  const response = await getPartnersController.handle();
-  res.send(response.body).status(response.statusCode);
-});
+  await MongoClient.connect();
 
-app.listen(port, () => console.log(`listening on port ${port}!`));
+  app.get("/partners", async (req, res) => {
+    const mngoGetPartnersRepository = new MongoGetPartnersRepository();
+    const getPartnersController = new GetPartnersControler(
+      mngoGetPartnersRepository
+    );
+    const response = await getPartnersController.handle();
+    res.send(response.body).status(response.statusCode);
+  });
+
+  const port = process.env.PORT || 8000;
+
+  app.listen(port, () => console.log(`listening on port ${port}!`));
+};
+
+main();
