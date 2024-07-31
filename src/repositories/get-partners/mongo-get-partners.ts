0b504ef8bd2d168c22ec688244b7d89cf892e9ca
Louran Costa <louran.costa@yahoo.com.br>
2024-07-31 06:40:59 -0300

    feat: ensure GetPartnersMongoRepository gets partners from mongo

--- src/repositories/get-partners/mongo-get-partners.ts
+++ src/repositories/get-partners/mongo-get-partners.ts
@@ -1,16 +1,17 @@
 import { IGetPartnersRepository } from "../../controllers/get-partners/protocols";
+import { MongoClient } from "../../database/mongo";
 import { Partner } from "../../model/partner";
 
 export class MongoGetPartnersRepository implements IGetPartnersRepository {
   async getPartners(): Promise<Partner[]> {
-    return [
-      {
-        name: "Louran Costa",
-        domain: "lourancosta.com",
-        phone: "213123123",
-        city: "Vancouver",
-        country: "Canada",
-      },
-    ];
+    const partners = await MongoClient.db
+      .collection<Omit<Partner, "id">>("partners")
+      .find({})
+      .toArray();
+
+    return partners.map(({ _id, ...rest }) => ({
+      ...rest,
+      id: _id.toHexString(),
+    }));
   }
 }
