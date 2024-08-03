e05903d761b598122f92f90ea782b7000aa19f01
Louran Costa <louran.costa@yahoo.com.br>
2024-08-03 12:03:30 -0300

    feat: script to do python report

--- src/analysis/data-analysis.py
+++ src/analysis/data-analysis.py
@@ -1,2 +1,39 @@
-import sys
-print ("Hello", str(sys.argv[1]))
+import requests
+import pandas as pd
+import json
+
+url = "http://localhost:8000/api/partners"
+req = requests.get(url, timeout=5)
+response = req.json()
+
+partners = {
+  'name': [],
+  'country': []
+}
+
+for partner in response:
+  name = partner.get("name")
+  partnersNames = partners.get('name')
+  partnersNames.append(name)
+
+  country = partner.get("country")
+  partnersCountries = partners.get('country')
+  partnersCountries.append(country)
+
+partner_df = pd.DataFrame(partners)
+print(partner_df)
+print('')
+
+amountPartnersByCountry = partner_df['country'].value_counts()
+print(amountPartnersByCountry)
+print('')
+
+report_df = pd.DataFrame(amountPartnersByCountry)
+jsonReport = report_df.to_json()
+
+def write_json(list):
+  with open('report.json', 'w') as f:
+    json.dump(list, f)
+
+write_json(jsonReport)
+
